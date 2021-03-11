a7fd8668f2aa1ce096e1beed45d5babf6625067a
HelloCore <aukwat@gmail.com>
2021-03-11 15:24:20 +0700

    Do nothing

--- dangerfile.dart
+++ dangerfile.dart
@@ -2,8 +2,6 @@ import 'package:danger_core/danger_core.dart';
 
 void main() {
   // No Rule for now
-  message('PR Title is ${danger.github.pr.title}');
-  message('PR number is ${danger.github.pr.number}');
 
   // warn('Try inline message',
   //     file: 'packages/danger_core/lib/src/mock_json_annotation.dart', line: 14);
