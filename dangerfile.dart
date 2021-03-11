f6988f55731a6edf915cc5d1680e65929fd6be62
HelloCore <aukwat@gmail.com>
2021-03-11 15:18:16 +0700

    Try rule

--- dangerfile.dart
+++ dangerfile.dart
@@ -2,8 +2,9 @@ import 'package:danger_core/danger_core.dart';
 
 void main() {
   // No Rule for now
+  message('PR Title is ${danger.github.pr.title}');
+  message('PR number is ${danger.github.pr.number}');
 
-  // message('Hello World');
   // warn('Try inline message',
   //     file: 'packages/danger_core/lib/src/mock_json_annotation.dart', line: 14);
   // warn('hello1 ${danger.bitbucketCloud?.pr?.title}');
