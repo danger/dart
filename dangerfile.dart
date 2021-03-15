d84009497ce6c74e0c3f170cd66193c853e76e6c
HelloCore <aukwat@gmail.com>
2021-03-15 15:26:42 +0700

    Remove dependencies

--- dangerfile.dart
+++ dangerfile.dart
@@ -1,7 +1,12 @@
+// @dart=2.10
 import 'package:danger_core/danger_core.dart';
 
 void main(List<String> args, dynamic data) {
   Danger.setup(data);
+
+  if (danger.github == null) {
+    message('Welcome to danger local');
+  }
 
   if (danger.github?.pr?.title?.contains('WIP') == true) {
     warn('PR is considered WIP');
