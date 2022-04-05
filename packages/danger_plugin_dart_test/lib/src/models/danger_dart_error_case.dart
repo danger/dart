0aabaeb188d6dfc20d684b2ce50ba61011ea0454
HelloCore <aukwat@gmail.com>
2022-04-05 12:49:55 +0700

    feat: migrate danger_plugin_dart_test to null-safety

--- packages/danger_plugin_dart_test/lib/src/models/danger_dart_error_case.dart
+++ packages/danger_plugin_dart_test/lib/src/models/danger_dart_error_case.dart
@@ -4,5 +4,9 @@ class DangerDartErrorCase {
   final String testName;
   final String message;
 
-  DangerDartErrorCase({this.fileName, this.lineNo, this.testName, this.message});
+  DangerDartErrorCase(
+      {required this.fileName,
+      required this.lineNo,
+      required this.testName,
+      required this.message});
 }
